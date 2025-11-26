9479eef72343f25a65a5a9ab7d7fb255ba86ee7b
Raju18 <rajualaveni7893@gmail.com>
2025-11-26 23:16:30 +0530

    Remove console log from data fetching
    Remove console log of fetched data

--- src/components/nave.jsx
+++ src/components/nave.jsx
@@ -13,7 +13,6 @@ function nave() {
                 credentials:'include'
             })
             const data = await res.json()
-            console.log(data)
         }catch(err){
             console.log(err)
         }
