59136b5408898b07ba65ddbbc490b058a65ea1a5
Raju18 <rajualaveni7893@gmail.com>
2025-11-26 23:10:35 +0530

    Update login.jsx

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
                 credentials: "include"
             })
             const reqRes = await res.json()
-            console.log(reqRes.status)
+            console.log(reqRes.username)
             if (res.ok) {
                 e.target.reset()
                 setEmailErr('')
