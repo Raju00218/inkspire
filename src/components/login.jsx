a4ba05ca90ff39145a91dfb01b0d1816197137cf
Raju18 <rajualaveni7893@gmail.com>
2025-11-26 23:22:51 +0530

    Update console log for username response

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
                 credentials: "include"
             })
             const reqRes = await res.json()
-                console.log(reqRes.username)
+                console.log('username',reqRes.username)
             if (res.ok) {
                 e.target.reset()
                 setEmailErr('')
