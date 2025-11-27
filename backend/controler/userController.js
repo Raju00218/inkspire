5fbf127c6da84526668502406ca55a94173d802c
Raju18 <rajualaveni7893@gmail.com>
2025-11-27 20:07:28 +0530

    Update userController.js

--- backend/controler/userController.js
+++ backend/controler/userController.js
@@ -84,14 +84,14 @@ const login_get=(req,res)=>{
     });
 }
 const logout_get = (req, res) => {
-    consol.log(req.cookies)
+    console.log(req.cookies
     res.clearCookie('info',{
                             httpOnly: true,
                             sameSite: "none",
                             path: "/" ,
                             secure: true,
                            });
-  consol.log(res.clearCookie())
+  console.log(res.clearCookie())
     res.redirect('/');
 };
 
