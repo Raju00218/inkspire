c8f99ce008e7f19279697bec1b01dea4840cfb3b
Raju18 <rajualaveni7893@gmail.com>
2025-11-27 12:37:21 +0530

    Update pages.jsx

--- src/pages.jsx
+++ src/pages.jsx
@@ -13,7 +13,7 @@ function RoutePage(params) {
        <>
             <Routes>
              <Route path="/" element={<App />} />
-             <Route path="/article_editer" 
+             <Route path="/article_editor" 
               element={
               <ProtectedRoutes>
                     <ArticleEditer />
