09517b3282d00c09759418da4d74ca2ab3806fb0
Raju18 <rajualaveni7893@gmail.com>
2025-11-27 10:55:29 +0530

    Fix cookie domain formatting in userController

--- backend/controler/userController.js
+++ backend/controler/userController.js
@@ -62,7 +62,7 @@ const login_post = async (req, res) => {
         const token = jwtoken(user._id)
         res.cookie('info', token, { httpOnly: true, secure: true, 
                                    maxAge:maxAge*1000, 
-                                   domain :"inkspire-7yk5.onrender.com"
+                                   domain :"inkspire-7yk5.onrender.com",
                                    path: "/"
                                   })
         res.status(201).json({ 
