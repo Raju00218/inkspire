44d6ad4b44d3de5c43a59732ff1eea602bc4157c
Raju18 <rajualaveni7893@gmail.com>
2025-12-01 23:43:28 +0530

    added some time to navigate when refresh

--- src/protectedRouets/protectRoutes.jsx
+++ src/protectedRouets/protectRoutes.jsx
@@ -4,11 +4,11 @@ import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoutes({children}) {
     const { username, setLoginPop } = useContext(Context)
-
-    if(!username){
-      // setLoginPop(true)
-      return <Navigate to ='/'/>
-
-    }
+    setTimeout(()=>{
+      if (!username) {
+        setLoginPop(true)
+        return <Navigate to='/' />
+      }
+    },300)
 return children
 }
