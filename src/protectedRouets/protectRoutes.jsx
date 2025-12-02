413291a24d5f9fecb33eccd6932d0cde0fe9b18b
Raju18 <rajualaveni7893@gmail.com>
2025-12-02 11:59:32 +0530

    added favicon

--- src/protectedRouets/protectRoutes.jsx
+++ src/protectedRouets/protectRoutes.jsx
@@ -4,6 +4,7 @@ import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoutes({children}) {
     const { username, setLoginPop } = useContext(Context)
+
       if (!username) {
         setLoginPop(true)
         return <Navigate to='/' />
